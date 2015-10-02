ec4894798f86d1dd35bea230b1c300c0ba239ed4
Brian <bman.brianthai@gmail.com>
2015-10-01 19:49:22 -0600

    Inject Environment Variables
    Added mapping for system environment variables into the web server process

--- src/main/java/org/jenkinsci/plugins/phpwebserver/beans/PhpWebserver.java
+++ src/main/java/org/jenkinsci/plugins/phpwebserver/beans/PhpWebserver.java
@@ -3,6 +3,7 @@ package org.jenkinsci.plugins.phpwebserver.beans;
 import java.io.File;
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.util.Map;
 
 import org.jenkinsci.plugins.phpwebserver.config.PhpWebserverInstallation;
 
@@ -24,7 +25,11 @@ public class PhpWebserver {
 			throw new IllegalStateException("Port "+port+" is already used");
 		}
 		String php = PhpWebserverInstallation.getDefaultInstallation().getPhpExe();
-		process = new ProcessBuilder(php, "--server", host+":"+port, "--docroot", root.getAbsolutePath()).start();
+		process = new ProcessBuilder(php, "--server", host+":"+port, "--docroot", root.getAbsolutePath());
+		Map<String, String> systemEnv = System.getenv();
+		Map<String, String> phpEnv = process.environment();
+		phpEnv.putAll(systemEnv);
+		process.start();
 	}
 
 	/**
